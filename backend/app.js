2b5e15e3c75d804b97b62ee5858f57a932207742
zack-lang <zack.lang@proton.me>
2024-05-28 10:19:58 +0100

    linting rules

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 // import authRoutes from './routes/authRoutes.js';
 // import chatRoutes from './routes/chatRoutes.js';
-
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-  // app.use('/api/auth', authRoutes);
+// app.use('/api/auth', authRoutes);
 // app.use('/api/chat', chatRoutes);
 
 export default app;
