63613cf3aaa5724775bb9f5d83b00131a08bae44
zouheir-barhoumi <zouheir.barhoumi@gmail.com>
2024-06-01 02:05:37 +0100

    chat socket implementation

--- backend/sockets/chatSocket.js
+++ backend/sockets/chatSocket.js
@@ -1,20 +1,50 @@
 import { Server } from "socket.io";
+import Message from "../models/message";
 
 export const chatSocket = (httpServer) => {
   const ioServer = new Server(httpServer, {
     cors: {
-      origin: "*",
+      origin: "*", // adjust thsi to match frontend's origin
     },
   });
 
   ioServer.on("connection", (socket) => {
-    console.log("New client connected");
-    socket.on("message", (message) => {
-      console.log(`Received message from client: ${message}`);
-      ioServer.emit("message", message);
+    console.log("New client connected", socket.id);
+    // Listen for 'message' events from clients
+    socket.on("message", async ({ chatId, sender, content }) => {
+      try {
+        const message = await Message.create({ chatId, sender, content });
+        ioServer.to(chatId).emit("newMessage", message);
+      } catch (error) {
+        console.log(`Error sending message: ${error}`);
+        return socket.emit("error", { message: "Internal server error" });
+      }
     });
+
+    socket.on("join", (chatId) => {
+      if (chatId) {
+        socket.join(chatId);
+        console.log(`User ${socket.id} joined room ${chatId}`);
+      }
+    });
+
+    socket.on("typing", (chatId) => {
+      if (chatId) {
+        socket.to(chatId).emit("typing", socket.id);
+      }
+    });
+
+    socket.on("stopTyping", ({ chatId, user }) => {
+      if (chatId) {
+        socket.to(chatId).emit("stopTyping", { user });
+      }
+    });
+
+    // Listen for 'disconnect' events
     socket.on("disconnect", () => {
       console.log("Client disconnected");
     });
   });
+
+  return ioServer;
 };
