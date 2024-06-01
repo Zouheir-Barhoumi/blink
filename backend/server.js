705835b903a8c73189ac7ab00a06c55a84fd7bff
zouheir-barhoumi <zouheir.barhoumi@gmail.com>
2024-06-01 02:09:02 +0100

    modified the server

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,9 @@
-import http from 'http';
-import { Server } from 'socket.io';
-import app from './app.js';
-import { chatSocket } from './sockets/chatSocket.js';
+import http from "http";
+import app from "./app.js";
+import { chatSocket } from "./sockets/chatSocket.js";
 
 const server = http.createServer(app);
-const io = new Server(server);
-
-io.on('connection', (socket) => {
-  console.log('User connected');
-  chatSocket(socket, io);
-});
+const io = chatSocket(server);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
